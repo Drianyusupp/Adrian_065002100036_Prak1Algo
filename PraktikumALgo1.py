e356c90f612dc015f83929383959dbf01a6518f9
Drianyusupp <91178655+Drianyusupp@users.noreply.github.com>
2021-09-22 13:55:24 +0700

    init

---
+++ PraktikumALgo1.py
@@ -0,0 +1,24 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = ("  @      @@      @@       @        @       @@     @")
+>>> b = ("@   @    @  @    @  @     @      @   @     @  @   @")
+>>> c = ("@@@@@    @  @    @@@      @      @@@@@     @   @  @")
+>>> d = ("@   @    @@      @  @     @      @   @     @    @@@")
+>>> print('Aku berkata padanya {} dia datang"{}!"'.format('di saat', 'JANGAN GANGGU AKU'))
+Aku berkata padanya di saat dia datang"JANGAN GANGGU AKU!"
+>>> print("I\'m Eating Now")
+I'm Eating Now
+>>> import math
+>>> x1=eval(input("masukkan nilai x1 : "))
+masukkan nilai x1 : ("5")
+>>> x2=eval(input("masukkan nilai x2 : "))
+masukkan nilai x2 : ("7")
+>>> y1=eval(input("masukkan nilai y1 : "))
+masukkan nilai y1 : ("9")
+>>> y2=eval(input("masukkan nilai y2 : "))
+masukkan nilai y2 : ("20")
+>>> print("jadi jarak antara titik A dan titik B adalah=",math.sqrt((x1-x2)**2+((y1-y2)**2)))
+SyntaxError: multiple statements found while compiling a single statement
+>>> print("185")
+185
+>>> 
